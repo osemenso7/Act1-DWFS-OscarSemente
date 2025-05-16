e7483123e0ad310f7f0f6d8c31cd636392eb419e
Óscar Sementé Solà <oscarsemente100@gmail.com>
2025-05-16 18:57:16 +0200

    Fix images

--- public/Data/BooksData.jsx
+++ public/Data/BooksData.jsx
@@ -4,7 +4,7 @@ export const books = [
         title: "Mistborn: El imperio final",
         author: "Brandon Sanderson",
         genres: ["Todos", "Fantasía", "Aventura"],
-        image: "src/Data/Images/Mistborn1.jpg",
+        image: "/Data/Images/Mistborn1.jpg",
         synopsis: "En un mundo donde la niebla cubre la tierra y los nobles gobiernan con mano de hierro, un grupo de rebeldes busca derrocar al tirano que los oprime. Con la ayuda de un misterioso joven, descubrirán secretos que cambiarán su destino.",
         price: 15.99,
         score: 4.5
@@ -14,7 +14,7 @@ export const books = [
         title: "Mistborn: El pozo de la ascensión",
         author: "Brandon Sanderson",
         genres: ["Todos", "Fantasía", "Aventura"],
-        image: "src/Data/Images/Mistborn2.jpg",
+        image: "/Data/Images/Mistborn2.jpg",
         synopsis: "Tras la caída del Lord Legislador, el mundo se enfrenta a nuevos peligros. Vin y sus amigos deben encontrar una forma de unir a las facciones en guerra y descubrir el verdadero poder del metal.",
         price: 17.99,
         score: 4.7
@@ -24,7 +24,7 @@ export const books = [
         title: "Mistborn: El héroe de las eras",
         author: "Brandon Sanderson",
         genres: ["Todos", "Fantasía", "Aventura"],
-        image: "src/Data/Images/Mistborn3.jpg",
+        image: "/Data/Images/Mistborn3.jpg",
         synopsis: "El mundo está al borde de la destrucción. Vin y Elend deben enfrentarse a sus propios demonios y descubrir el verdadero significado de la esperanza mientras luchan contra un enemigo que amenaza con consumir todo.",
         price: 19.99,
         score: 4.8
@@ -34,7 +34,7 @@ export const books = [
         title: "Romper el circulo",
         author: "Colleen Hoover",
         genres: ["Todos", "Romance", "Drama"],
-        image: "src/Data/Images/RomperElCirculo.jpg",
+        image: "/Data/Images/RomperElCirculo.jpg",
         synopsis: "Una historia de amor prohibido entre dos jóvenes que luchan contra sus propios demonios. A medida que se enamoran, deben enfrentarse a los secretos de su pasado y decidir si su amor es lo suficientemente fuerte para superar los obstáculos que se interponen en su camino.",
         price: 12.99,
         score: 4.6
@@ -44,7 +44,7 @@ export const books = [
         title: "El Resplandor",
         author: "Stephen King",
         genres: ["Todos", "Terror"],
-        image: "src/Data/Images/ElResplandor.jpg",
+        image: "/Data/Images/ElResplandor.jpg",
         synopsis: "Un escritor se convierte en el cuidador de un hotel aislado durante el invierno. A medida que la nieve cubre el mundo exterior, los oscuros secretos del hotel comienzan a revelarse, y la locura acecha en cada rincón.",
         price: 14.99,
         score: 4.4
@@ -54,7 +54,7 @@ export const books = [
         title: "Las 50 sombras de Grey",
         author: "E.L. James",
         genres: ["Todos", "Romance"],
-        image: "src/Data/Images/50Sombras.jpg",
+        image: "/Data/Images/50Sombras.jpg",
         synopsis: "Una joven estudiante de literatura se ve envuelta en un, apasionado y peligroso romance con un joven empresario multimillonario. A medida que su relación se intensifica, descubre un mundo de deseos ocultos y secretos oscuros.",
         price: 13.99,
         score: 4.3
@@ -64,7 +64,7 @@ export const books = [
         title: "Y todo eso, Una comedia profética",
         author: "Rose Macaulay",
         genres: ["Todos", "Comedia"],
-        image: "src/Data/Images/YTodoEso.jpg",
+        image: "/Data/Images/YTodoEso.jpg",
         synopsis: "Una sátira mordaz sobre la sociedad británica de la década de 1930, donde una joven escritora se enfrenta a las convenciones sociales y busca su lugar en un mundo lleno de hipocresía y superficialidad.",
         price: 11.99,
         score: 4.2
@@ -74,7 +74,7 @@ export const books = [
         title: "Volver a empezar",
         author: "Colleen Hoover",
         genres: ["Todos", "Romance", "Drama"],
-        image: "src/Data/Images/VolverAEmpezar.jpg",
+        image: "/Data/Images/VolverAEmpezar.jpg",
         synopsis: "Una historia conmovedora sobre el amor, la pérdida y la redención. Dos almas perdidas se encuentran en un momento crucial de sus vidas y deben aprender a sanar sus heridas mientras luchan por un futuro juntos.",
         price: 12.99,
         score: 4.5
@@ -84,7 +84,7 @@ export const books = [
         title: "Call Me by Your Name",
         author: "André Aciman",
         genres: ["Todos", "Romance", "Drama"],
-        image: "src/Data/Images/CallMeByYourName.jpg",
+        image: "/Data/Images/CallMeByYourName.jpg",
         synopsis: "Una historia de amor entre un joven y un estudiante de intercambio en la Italia de los años 80. A medida que su relación se desarrolla, deben enfrentarse a sus propios deseos y a las expectativas de la sociedad.",
         price: 13.99,
         score: 4.6
@@ -94,7 +94,7 @@ export const books = [
         title: "Find Me",
         author: "André Aciman",
         genres: ["Todos", "Romance", "Drama"],
-        image: "src/Data/Images/FindMe.jpg",
+        image: "/Data/Images/FindMe.jpg",
         synopsis: "Una secuela, donde los personajes principales se reencuentran años después de su primer encuentro. A medida que reviven su amor, deben enfrentarse a las decisiones que tomaron en el pasado y a las consecuencias de sus elecciones.",
         price: 14.99,
         score: 4.7
--- src/Context/GlobalContext.jsx
+++ src/Context/GlobalContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState } from 'react';
-import { books as books } from '../Data/BooksData';
+import { books as books } from '../../public/Data/BooksData';
 
 import useBuyBooks from "../Hooks/useBuyBooks.jsx";
 
